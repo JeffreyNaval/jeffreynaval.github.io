ef73e119e09cbb1f113e3c7f1ef8af4e2322d2ca
Jeffrey Naval <jeffrey.naval@outlook.com>
2015-07-29 20:06:44 +0800

    update layout

---
+++ _drafts/2015-07-29-setting-gulp-bower-bootstrap-sass-fontawesome.md
@@ -0,0 +1,10 @@
+---
+title: Setting Gulp, Bower, Bootstrap, Sass, Fontawesome
+---
+
+I've been searching for a quick setup guide on how to setup `Gulp`, `Bower`, `Bootstrap`, `Sass` and `Fontawesome`.
+Then I found [Eric L. Barnes blog](http://ericlbarnes.com/setting-gulp-bower-bootstrap-sass-fontawesome/).
+The problem is it's outdated and some settings are not available like `loadPath` on `Sass`.
+And `bootstrap-sass-official` is now `bootstrap-sass`.
+
+@TODO point how to use .bowerrc to copy fonts after bower install.
--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,67 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 
-// Sass
-gulp.task('sass', function () {
-  gulp.src('./src/sass/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'})
-        .on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'));
+// Config
+var config = {
+  bowerDir: './bower_components',
+  jsPath: './src/js',
+  sassPath: './src/sass',
+  outputPath: './assets'
+}
+
+// Bower Files
+var bowerFiles = {
+  js: [
+    config.bowerDir + '/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+    config.bowerDir + '/jquery/dist/jquery.min.js'
+  ],
+  fonts: [
+    config.bowerDir + '/fontawesome/fonts/**.*',
+    config.bowerDir + '/bootstrap-sass/assets/fonts/**/**.*'
+  ],
+  sass: [
+    config.bowerDir + '/bootstrap-sass/assets/stylesheets',
+    config.bowerDir + '/fontawesome/scss'
+  ]
+}
+
+// Copy Fonts
+gulp.task('copy:fonts', function() {
+    return gulp.src(bowerFiles.fonts)
+      .pipe(gulp.dest(config.outputPath+'/fonts'));
+});
+// Copy Javascripts
+gulp.task('copy:js', function() {
+    return gulp.src(bowerFiles.js)
+      .pipe(gulp.dest(config.outputPath+'/js'));
 });
 
+// Sass
+gulp.task('sass', function () {
+  gulp.src(config.sassPath+'/**/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed',
+      includePaths: bowerFiles.sass
+    }).on('error', sass.logError))
+    .pipe(gulp.dest(config.outputPath+'/css'));
+});
+// Sass Watch
 gulp.task('sass:watch', function () {
-  gulp.watch('./src/sass/**/*.scss', ['sass']);
+  gulp.watch(config.sassPath+'/**/*.scss', ['sass']);
 });
 
 // Uglify
 gulp.task('uglify', function() {
-  return gulp.src('./js/*.js')
+  return gulp.src(config.jsPath+'/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('./assets/js'));
+    .pipe(gulp.dest(config.outputPath+'/js'));
+});
+// Uglify Watch
+gulp.task('uglify:watch', function () {
+  gulp.watch(config.jsPath+'/*.js', ['uglify']);
 });
 
 // Tasks
 gulp.task('default', ['sass','uglify']);
-gulp.task('watch', function () {
-    gulp.watch('./src/sass/**/*.scss', ['sass']);
-    gulp.watch('./src/js/*.js', ['uglify']);
-});
+gulp.task('watch', ['sass:watch','uglify:watch']);
+gulp.task('copy',['copy:js','copy:fonts']);
